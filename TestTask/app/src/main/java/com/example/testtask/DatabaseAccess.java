fb2484bed0b335bb5c0a7d6c9a63edf43f2bb55e
misterbee180 <misterbee180@gmail.com>
2017-08-17 22:12:40 -0600

    Revert "Test"
    This reverts commit d97d7490370619800ed7c9d08ff00e0cde4512f9.

--- TestTask/app/src/main/java/com/example/testtask/DatabaseAccess.java
+++ TestTask/app/src/main/java/com/example/testtask/DatabaseAccess.java
@@ -104,7 +104,7 @@ public class DatabaseAccess {
         //endregion
 
         TaskDatabaseHelper(Context context) {
-            super(context, "TaskDatabase.db", null, 999);
+            super(context, "TaskDatabase.db", null, 2);
         }
 
         @Override
@@ -142,15 +142,13 @@ public class DatabaseAccess {
                               int oldVersion,
                               int newVersion){
 
-            if (newVersion == 998) {
-                db.execSQL(DROP_TASK_TABLE);
-                db.execSQL(DROP_TASKINSTANCE_TABLE);
-                db.execSQL(DROP_SESSION_TABLE);
-                db.execSQL(DROP_TIME_TABLE);
-                db.execSQL(DROP_WEEK_TABLE);
-                db.execSQL(DROP_EVENT_TABLE);
-                onCreate(db);
-            }
+            db.execSQL(DROP_TASK_TABLE);
+            db.execSQL(DROP_TASKINSTANCE_TABLE);
+            db.execSQL(DROP_SESSION_TABLE);
+            db.execSQL(DROP_TIME_TABLE);
+            db.execSQL(DROP_WEEK_TABLE);
+            db.execSQL(DROP_EVENT_TABLE);
+            onCreate(db);
         }
 
         private void upgradeToV2(SQLiteDatabase db){
